03af93b2bd1446e74af77214a3b7ab17b03f3338
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-10 16:46:55 +0100

    add solution

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
-
+  const [page, setPage] = useState(0); /*wir starten bei Seite 0!*/
+  console.log(page);
+  const limit = 20;
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${limit * page}`
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -17,12 +19,17 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [page]);
 
+  function handleNextPage() {
+    setPage(page + 1);
+  }
   return (
     <main>
       <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={handleNextPage}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
